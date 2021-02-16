5a8ff26964d653cd44bf01fb2f36f067574c2a88
sinan akalin <71410874+sinanakal@users.noreply.github.com>
2021-02-17 01:32:17 +0200

    last updated

--- src/test/java/com/fidexio/runners/CukesRunner.java
+++ src/test/java/com/fidexio/runners/CukesRunner.java
@@ -7,10 +7,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features= "src/test/resources/features",
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports", "rerun:target/rerun.txt"},
+        features = "src/test/resources/features/",
         glue = "com/fidexio/step_definitions",
-        dryRun = false,
-        tags = "@driver"
+        dryRun =false,
+        tags = "@Login"
 )
 public class CukesRunner {
 }
