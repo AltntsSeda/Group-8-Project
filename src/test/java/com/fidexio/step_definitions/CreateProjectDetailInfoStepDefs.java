46948fddb24cd2227b79b02df83c5306ed91fcdb
AltntsSeda <76876800+AltntsSeda@users.noreply.github.com>
2021-03-08 12:42:06 +0200

    Updated and finished Create Project with Detailed Info.

--- src/test/java/com/fidexio/step_definitions/CreateProjectDetailInfoStepDefs.java
+++ src/test/java/com/fidexio/step_definitions/CreateProjectDetailInfoStepDefs.java
@@ -2,65 +2,73 @@ package com.fidexio.step_definitions;
 
 import com.fidexio.pages.BasePage;
 import com.fidexio.pages.DashboardPage;
+import com.fidexio.pages.ProjectPage;
+import com.fidexio.utilities.BrowserUtils;
+import com.fidexio.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
 
 public class CreateProjectDetailInfoStepDefs {
+
+    ProjectPage projectPage = new ProjectPage();
+    DashboardPage dashboardPage = new DashboardPage();
+
     @Given("the user navigates to {string} tab")
     public void the_user_navigates_to_tab(String tab) throws InterruptedException {
-        new DashboardPage().navigateTo(tab);
-        new DashboardPage().waitUntilScreenLoaderDisappear();
+        projectPage.navigateTo(tab);
+        dashboardPage.waitUntilScreenLoaderDisappear();
+        Thread.sleep(300);
+    }
 
+    @Then("the user click Create button on Project page")
+    public void the_user_click_Create_button_on_Project_page() {
+        projectPage.createButton.click();
+        dashboardPage.waitUntilScreenLoaderDisappear();
     }
 
 
-    @Then("the user click {string} button on Project page")
-    public void the_user_click_button_on_Project_page(String string) {
-
-
-    }
     @Then("the user able to write {string} on Project Name box")
     public void the_user_able_to_write_on_Project_Name_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        projectPage.projectNameBox.sendKeys(string);
+
     }
-    @Then("the user able to write {string} on Choose a Project Email box")
+
+    @And("the user able to write {string} on Choose a Project Email box")
     public void the_user_able_to_write_on_Choose_a_Project_Email_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able to click {string} button")
-    public void the_user_able_to_click_button(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able to write {string} on Customer box")
-    public void the_user_able_to_write_on_Customer_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able select {string} dropdown on Customer box")
-    public void the_user_able_select_dropdown_on_Customer_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able to click {string} button on the Projects page tab")
-    public void the_user_able_to_click_button_on_the_Projects_page_tab(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able to search {string} on serach box as Project Name")
-    public void the_user_able_to_search_on_serach_box_as_Project_Name(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the user able to see {string} Kanban on Project Dashboard search page")
-    public void the_user_able_to_see_Kanban_on_Project_Dashboard_search_page(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        projectPage.ChooseProjectEmailBox.sendKeys(string);
+
     }
 
+    @Then("the user able to click Create&Edit button")
+    public void the_user_able_to_click_Create_Edit_button(){
 
+        projectPage.createAndEditButton.click();
+    }
 
+    @Then("select a customer from the customer dropdown {string}")
+    public void select_a_customer_from_the_customer_dropdown(String customerName) {
+
+        BrowserUtils.waitFor(1);
+        projectPage.selectACustomerFromCustomerDropdown(customerName);
+    }
+
+    @Then("the user able to click Save button on the Projects page tab")
+    public void the_user_able_to_click_Save_button_on_the_Projects_page_tab() {
+
+        BrowserUtils.waitFor(1);
+        projectPage.saveButtonOnProjectsPage.click();
+        BrowserUtils.waitFor(1);
+        projectPage.navigateTo("Project");
+
+    }
+    @Then("the user should see one additional project with the new project name {string}")
+    public void the_user_should_see_one_additional_project_with_the_new_project_name(String projectName) {
+
+        projectPage.verifyThatTheNumberOfProjectsWithNewProjectNameIncreasedByOne(projectName);
+
+    }
 }
--- src/test/resources/features/CreateProjectDetailedInfo.feature
+++ src/test/resources/features/CreateProjectDetailedInfo.feature
@@ -1,11 +1,8 @@
-Feature:
+Feature: Create Project with Detailed Information
 
 	Background:
-		#@FID-208
 		When the user enter valid credentials "posmanager16@info.com" "posmanager"
 		Then the user should be able to login
-
-
 
 	#User Story: As a POS Manager I should be able to create new project with detailed information
 	#
@@ -20,13 +17,11 @@ Feature:
 	@FID-206
 	Scenario: Create Project with Detailed Information (Aut.) Test
 		And the user navigates to "Project" tab
-		Then the user click "Create" button on Project page
+		Then the user click Create button on Project page
 		Then the user able to write "Project EU4" on Project Name box
 		And the user able to write "eu4testmail" on Choose a Project Email box
-		Then the user able to click "Create&Edit" button
-		Then the user able to write "Seda" on Customer box
-		And the user able select "Create" dropdown on Customer box
-		And the user able to click "Save" button on the Projects page tab
-		And the user able to search "Project EU4" on serach box as Project Name
-		Then the user able to see "Project EU4" Kanban on Project Dashboard search page
+		Then the user able to click Create&Edit button
+		Then select a customer from the customer dropdown "0000"
+		Then the user able to click Save button on the Projects page tab
+		Then the user should see one additional project with the new project name "Project EU4"
 		
