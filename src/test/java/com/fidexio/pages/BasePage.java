7fd27230fedae084d6e79a23508a273e22cf871c
sinan akalin <71410874+sinanakal@users.noreply.github.com>
2021-03-10 01:47:10 +0200

    last updated 4

--- src/test/java/com/fidexio/pages/BasePage.java
+++ src/test/java/com/fidexio/pages/BasePage.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.Set;
 
 public abstract class BasePage {
 
@@ -41,6 +42,21 @@ public abstract class BasePage {
         @FindBy(xpath = "//div[@class='o_loading']")
         public WebElement screenLoader;
 
+    @FindBy(xpath = "//h1[contains(text(),'Odoo User Documentation')]")
+    public WebElement documentationPageLocator;
+
+    @FindBy(xpath = "//h1[contains(text(),'Odoo Pricing')]")
+    public WebElement supportPageLocator;
+
+    @FindBy(xpath = "//span[contains(text(),'Cancel')]")
+    public WebElement preferencesPageLocator;
+
+    @FindBy(xpath = "//a[contains(text(),'Sign in')]")
+    public WebElement myOdooComAccountPageLocator;
+
+    @FindBy(xpath = "//button[contains(text(),'Log in')]")
+    public WebElement loginPageLocator;
+
         public void navigateTo (String tab){
             String tabLoc = "//span[normalize-space()='" + tab + "' and contains(@class, 'oe_menu_text')]";
             WebElement module = Driver.get().findElement(By.xpath(tabLoc));
@@ -55,8 +71,54 @@ public abstract class BasePage {
             new WebDriverWait(Driver.get(), 10).until(ExpectedConditions.visibilityOf(screenLoader));
             new WebDriverWait(Driver.get(), 10).until(ExpectedConditions.invisibilityOf(screenLoader));
         }
-
+    public void clickOnTheUsername(){
+        BrowserUtils.waitFor(3);
+        topUsername.click();
     }
+
+    public void clickOnTheDropdownItem(String dropdownItem){
+        BrowserUtils.waitFor(1);
+        String locator = "//a[contains(text(),'" + dropdownItem + "')]";
+        Driver.get().findElement(By.xpath(locator)).click();
+    }
+
+    public void verifyYouAreOnTheRelatedPage(String dropdownItem){
+        BrowserUtils.waitFor(1);
+        if (dropdownItem.contains("Documentation")) {
+            swtichToTheNewPage();
+            Assert.assertTrue(documentationPageLocator.isDisplayed());
+        }
+        if (dropdownItem.contains("Support")) {
+            swtichToTheNewPage();
+            Assert.assertTrue(supportPageLocator.isDisplayed());
+        }
+        if (dropdownItem.contains("Preferences")) {
+            Assert.assertTrue(preferencesPageLocator.isDisplayed());
+            preferencesPageLocator.click();
+        }
+        if (dropdownItem.contains("My Odoo.com account")) {
+            Assert.assertTrue(myOdooComAccountPageLocator.isDisplayed());
+            Driver.get().navigate().back();
+        }
+        if (dropdownItem.contains("Log out")) {
+            Assert.assertTrue(loginPageLocator.isDisplayed());
+        }
+    }
+
+    public void swtichToTheNewPage(){
+        BrowserUtils.waitFor(1);
+        String currentWindowHandle = Driver.get().getWindowHandle();
+        Set<String> windowHandles = Driver.get().getWindowHandles();
+        for (String each : windowHandles) {
+            if (!each.equals(currentWindowHandle)) {
+                Driver.get().switchTo().window(each);
+            }
+        }
+    }
+
+}
+
+
 
 
 
--- src/test/java/com/fidexio/step_definitions/UserNameStepsDefs.java
+++ src/test/java/com/fidexio/step_definitions/UserNameStepsDefs.java
@@ -8,9 +8,16 @@ import com.fidexio.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class UserNameStepsDefs {
 
+   DashboardPage dashboardPage = new DashboardPage();
 
    @When("the user enter valid credentials {string} {string}")
    public void enter_valid_credentials(String email, String password) {
@@ -26,7 +33,16 @@ public class UserNameStepsDefs {
 
    }
 
-}
+
+   }
+
+
+
+
+
+
+
+
 
 
 
